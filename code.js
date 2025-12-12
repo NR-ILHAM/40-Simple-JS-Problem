f18e92eadf39f9ef9a164d03ab37bc071d835b67
unknown <nrilhamttp13@gmail.com>
2025-12-12 17:39:03 +0300

    Updated Files

--- code.js
+++ code.js
@@ -24,3 +24,14 @@ console.log("Total :", count);
 
 // 3rd Problem >>  অ্যারে থেকে সর্বোচ্চ সংখ্যাটি খুঁজে বের করা
 
+const arr = [4, 2, 9, 1, 7];
+let max = arr[0];
+
+for (let i = 1; i < arr.length; i++) {
+  if (arr[i] > max) {
+    max = arr[i];
+  }
+}
+
+console.log("Total length of array : " ,arr.length);//length count korbe
+console.log("Max:", max);
