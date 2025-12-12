509801832229a6595450659bd68745ea3fe6a804
unknown <nrilhamttp13@gmail.com>
2025-12-12 17:02:35 +0300

    Initial commit

---
+++ 1st.js
@@ -0,0 +1,26 @@
+// 1st problem > ভ্যারিয়েবল, কনসোল, কন্ডিশন
+
+const age = 17; //aikane const variable ta dara bujai man change kora jabe
+
+if (age >= 18)
+{
+    console.log("Adult");
+} else
+{
+        console.log(" Not Adult");
+
+}
+
+// 2nd problem >> লুপ দিয়ে কাউন্ট করা
+
+let count = 0;
+
+for(let i =1; i<=20; i++)
+{
+    count=count + i;
+
+}
+console.log("Total :", count);
+
+// 3rd Problem >>  অ্যারে থেকে সর্বোচ্চ সংখ্যাটি খুঁজে বের করা
+
